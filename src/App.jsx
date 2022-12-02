dd32e996bb3ba0d94d2599df0289383844a2c24b
Brittney Brinson <brittneyc95@outlook.com>
2022-12-01 22:39:53 -0500

    Learning about JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const title = "React";
 function App() {
+
   return (
     <div>
-        <h1>Hello,World</h1>
+        <h1>Hello {title} </h1>  {/* Title was displayed to the browser using Javascript XML (JSX)*/}
         <h2>Brittney is an amazing software developer</h2>
+
     </div>
   );
 }
