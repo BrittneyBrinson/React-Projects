db938f9d9cf92ddebc8042d810e0043b4233fbba
Brittney Brinson <brittneyc95@outlook.com>
2022-12-06 21:29:43 -0500

    Rendered some html to the browser using Javascript Object

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-const title = "React";
+const welcome = {
+    greeting: "Hey",
+    title: " React",
+};
 function App() {
 
   return (
     <div>
-        <h1>Hello {title} </h1>  {/* Title was displayed to the browser using Javascript XML (JSX)*/}
+        <h1>{welcome.greeting}{welcome.title} </h1>  {/* Title was displayed to the browser using Javascript XML (JSX)*/}
         <h2>Brittney is an amazing software developer</h2>
+
+        <label htmlFor="search">Search</label>
+        <input id="search" type="text"/>
 
     </div>
   );
